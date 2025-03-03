5e5b82145b58b2d2fcf3302212aa420378456449
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-03 18:32:03 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,25 +3,31 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	public static int sum2D(int[][]arr2D){
+		int sum = 0;
+		for (int row = 0; row<arr2D.length;row++){
+			for (int col = 0; col<arr2D[row].length;col++){
+				sum +=arr2D[row][col];
+			}
+		
+		}
+		return sum;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int sumRow(int[][] arr2D1, int row){
+		int sum = 0;
+			for (int col = 0; col<arr2D1[row].length;col++){
+				sum +=arr2D1[row][col];
+			}
+		return sum;
 	}
+public static int sumColumn(int[][] arr2D2, int col){
+	int sum = 0;
+		for (int row = 0; row<arr2D2.length;row++){
+			sum +=arr2D2[row][col];
+		}
+	return sum;
+}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 }
